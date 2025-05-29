d6566f1e60838f138fdbc39900330911d7ec1237
meuse-robotics <123723218+meuse-robotics@users.noreply.github.com>
2025-05-29 20:50:21 +0900

    pigpio >> gpiozero

---
+++ ros2_basic/raspi/chap4/motor_drive.py
@@ -0,0 +1,11 @@
+from gpiozero import PWMOutputDevice #RaspiのGPIOを使うためのライブラリ
+from time import sleep # sleepを使う
+
+MOT_R_1 = PWMOutputDevice(pin=23, frequency=60) #GPIO23 60HzでPWM
+MOT_R_2 = PWMOutputDevice(pin=22, frequency=60) #GPIO22 60HzでPWM
+
+MOT_R_1.value = 0.5 #duty50%でPWM出力
+MOT_R_2.value = 0
+sleep(5)
+MOT_R_1.value = 0 #PWM出力停止
+MOT_R_2.value = 0
